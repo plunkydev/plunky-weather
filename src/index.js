99acee962f093b8b30ccf3cd9b94f931e4c94afb
plunkydev <plunkydesign@gmail.com>
2025-03-07 15:44:02 -0500

    Se eliminó import innecesario, se agregó import de estilos, se obtienen elementos de búsqueda, y se agregó evento click para capturar ciudad ingresada y obtener datos de clima con getWeather

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-import { copyData } from "./data.js";
+import "./styles.css"
 
+import { getWeather } from "./data.js";
+
+
+
+const busquedaInput = document.getElementById('busquedaId');
+const busquedaBoton = document.getElementById('busquedaBotonId');
+
+busquedaBoton.addEventListener('click', () => {
+    const location = {
+        latitud: 0,
+        longitud: 0,
+        city: "",
+    }
+    location.city = busquedaInput.value;
+    getWeather(location);
+});
