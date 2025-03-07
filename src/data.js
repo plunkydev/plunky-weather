a52f6774a4cef0a4b4f80aa7d14d1ba93e931f28
plunkydev <plunkydesign@gmail.com>
2025-03-07 15:45:44 -0500

    Se mejoró la función getWeather agregando manejo de errores y renderizado dinámico con render; se integró la ubicación del usuario directamente al flujo de obtención de clima; se modificó getGif para mostrar imagen de meme directamente en el contenedor memeContainer; se eliminó el uso de fondo fijo y se corrigieron propiedades para asegurar correcta visualización

--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
+import { render } from "./domRenderer.js"
 const giphiApiKey = "Ka0KnVifxMeN2QhIf7amGnIfoRTY8VU0"
-const keyApi = "0351861a5dca300b32e11fa9761d7e1c"
+const keyApi = "aee6db3ecfc67b679ceccb6c1394c963"
 
 const location = {
     latitud: 0,
@@ -11,48 +12,66 @@ const options = {
     method: "GET",
 };
 
-export async function getWeather(currentLocation) {
-    if (location.city){
-        const response = await fetch(`http://api.weatherstack.com/current?access_key=${keyApi}&query=${currentLocation.city}`, options)
-        const data = await response.json()
-        console.log(data)
-        const datosFiltrados = {
-            lugar: `${data.location.name} ${data.location.region}, ${data.location.country}`,
-            icons: data.current.weather_icons,
-            descripcion: data.current.weather_descriptions,
-            temperatura: data.current.temperature,
-            viento: data.current.wind_speed,
-            presipitacion: data.current.precip,
-            precion: data.current.pressure
+async function getWeather(currentLocation) {
+    try {
+        if (currentLocation.city){
+            const response = await fetch(`http://api.weatherstack.com/current?access_key=${keyApi}&query=${currentLocation.city}`, options)
+            const data = await response.json()
+            console.log(data)
+            const datosFiltrados = {
+                lugar: `${data.location.name} ${data.location.region}, ${data.location.country}`,
+                icons: data.current.weather_icons,
+                descripcion: data.current.weather_descriptions,
+                temperatura: data.current.temperature,
+                viento: data.current.wind_speed,
+                presipitacion: data.current.precip,
+                presion: data.current.pressure
+            }
+            console.log(datosFiltrados)
+            getGif(data.current.weather_descriptions[0])
+            render(datosFiltrados)
+        } else {
+            const response = await fetch(`http://api.weatherstack.com/current?access_key=${keyApi}&query=${currentLocation.latitud},${currentLocation.longitud}`, options)
+            const data = await response.json()
+            console.log(data)
+            const datosFiltrados = {
+                lugar: `${data.location.name} ${data.location.region}, ${data.location.country}`,
+                icons: data.current.weather_icons,
+                descripcion: data.current.weather_descriptions,
+                temperatura: data.current.temperature,
+                viento: data.current.wind_speed,
+                presipitacion: data.current.precip,
+                presion: data.current.pressure
+            }
+            console.log(datosFiltrados)
+            getGif(data.current.weather_descriptions[0])
+            render(datosFiltrados)
         }
-        console.log(datosFiltrados)
-        getGif(data.current.weather_descriptions[0])
-    } else {
-        const response = await fetch(`http://api.weatherstack.com/current?access_key=${keyApi}&query=${currentLocation.latitud},${currentLocation.longitud}`, options)
-        const data = await response.json()
-        const datosFiltrados = {
-            lugar: `${data.location.name} ${data.location.region}, ${data.location.country}`,
-            icons: data.current.weather_icons[0],
-            descripcion: data.current.weather_descriptions,
-            temperatura: data.current.temperature,
-            viento: data.current.wind_speed,
-            presipitacion: data.current.precip,
-            precion: data.current.pressure
-        }
-        console.log(datosFiltrados)
-        getGif(data.current.weather_descriptions[0])
+    } catch (error) {
+        if (error) {
+            alert("Limite de consultas excedido por este mes");
+    }
     }
 }
+
+
 
 // Obtener la ubicación del usuario---------------------------------------------------------
 
 if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(
         (position) => {
-            const latitud =  position.coords.latitude;
-            const longitud =  position.coords.longitude;
-            location.latitud = latitud;
-            location.longitud = longitud;
+            if (position) {
+                const latitud =  position.coords.latitude;
+                const longitud =  position.coords.longitude;
+                location.latitud = latitud;
+                location.longitud = longitud;
+                console.log(location)
+                getWeather(location)
+            } else {
+                getWeather(location)
+                alert("No se pudo obtener la ubicación del usuario.");
+            }
         },
         (error) => {
             switch (error.code) {
@@ -76,36 +95,32 @@ if ("geolocation" in navigator) {
 }
 
 
-getWeather(location)
+/* getWeather(location) */
 
 
 
-export async function getGif(currentWeather) {
+async function getGif(currentWeather) {
     const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${giphiApiKey}&q=${currentWeather}&limit=5`);
     const data = await response.json();
     let index = Math.floor(Math.random() * 5);
     const imageUrl = data.data[index].images['original'].url;
+    const memeContainer = document.getElementById('memeContainerId');
 
     // Crear un elemento div para aplicar el fondo y el filtro
-    const backgroundDiv = document.createElement('div');
-    backgroundDiv.style.backgroundImage = `url(${imageUrl})`;
-    backgroundDiv.style.backgroundSize = "cover";
-    backgroundDiv.style.backgroundRepeat = "no-repeat";
-    backgroundDiv.style.backgroundPosition = "center center";
-    backgroundDiv.style.backgroundAttachment = "fixed";
-    backgroundDiv.style.height = "100vh";
-    backgroundDiv.style.width = "100vw";
+    const image = document.createElement("img");
+    image.style.backgroundImage = `url(${imageUrl})`;
+    image.style.backgroundSize = "cover";
+    image.style.backgroundRepeat = "no-repeat";
+    image.style.backgroundPosition = "center center";
+    image.style.height = "100%";
+    image.style.width = "100%";
 
     // Aplicar el filtro de color
-    backgroundDiv.style.filter = "sepia(100%) hue-rotate(191deg) blur(7px)";
+    image.style.filter = "sepia(100%) hue-rotate(191deg)";
 
-    // Asegurarse de que el div cubra todo el cuerpo y esté detrás del contenido
-    backgroundDiv.style.position = "fixed";
-    backgroundDiv.style.top = "0";
-    backgroundDiv.style.left = "0";
-    backgroundDiv.style.zIndex = "-1";
-
-    // Añadir el div al body
-    document.body.appendChild(backgroundDiv);
+    // Añadir el div al memeContainer
+    memeContainer.innerHTML = "";
+    memeContainer.appendChild(image);
 }
 
+export { getWeather }
